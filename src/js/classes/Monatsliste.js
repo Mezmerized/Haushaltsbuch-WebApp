23dca3f1dfde21676cba4275ed60aff262862b1c
ProjectXundefined <projectxundefined@protonmail.com>
2022-11-30 18:15:50 +0100

    OOP Vorbereitung
    Vorbereitung Objektorientierte Programmieung

---
+++ src/js/classes/Monatsliste.js
@@ -0,0 +1,16 @@
+"use strict";
+/* 
+<article class="monatsliste">
+<!-- Titel -->
+<!--             <h2>
+    <span class="monat-jahr">Februar 2020</span>
+    <span class="monatsbilanz negativ">-326,84€</span>
+</h2> -->
+<!-- Eintrags-Liste -->
+</article> */
+
+class Monatsliste {
+
+
+
+}
---
+++ src/js/classes/Navigationsleiste.js
@@ -0,0 +1,12 @@
+"use strict";
+/* 
+<!-- Navigationsleiste -->
+<nav id="navigationsleiste">
+    <a href="index.html"><span id="markenname">Liqui-Planner</span></a>
+</nav> */
+
+class Navigationsleiste {
+
+
+
+}
